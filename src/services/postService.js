bbd3cf0bcefda5b67b9b8458bf7780fbf30767f2
Long Pham <longpham@sandiego.edu>
2024-07-09 00:30:42 -0700

    corrections

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com"
+  baseURL: "https://jsonplaceholder.typicode.com",
 });
 
-const getPosts = () => api.get("/posts");,
+const getPosts = () => api.get("/posts");
 const deletePost = (id) => api.delete(`/posts/${id}`);
 
-export {getPosts, deletePost};
+export { getPosts, deletePost };
