e2e286119d82a12b926935740ade682bdd29e4c2
Long Pham <longpham@sandiego.edu>
2024-07-09 14:29:34 -0700

    modifications

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Posts from "./components/Posts";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Posts></Posts>
+    </div>
+  );
 }
 
 export default App;
